6b946d85cfca2fd8e99da98ce20ee7420f6c0fcf
Pedro Ferreira <pedrohfs.2207@gmail.com>
2026-04-15 09:22:39 -0300

    desafio feito

--- script/controles-foto-anotada.js
+++ script/controles-foto-anotada.js
@@ -5,3 +5,16 @@ const imgFoto = document.querySelector('.foto-anotada img');
 selectFiltro.addEventListener('change', function () {
   imgFoto.style.filter = selectFiltro.value;
 });
+
+//desafio 1
+document.querySelector('#imagem').addEventListener('change', function (e) {
+  const reader = new FileReader();
+
+  reader.addEventListener('load', function () {
+    imgFoto.src = reader.result;
+    imgFoto.style.filter = 'none';
+    selectFiltro.value = 'none';
+  });
+
+  reader.readAsDataURL(e.target.files[0]);
+});
